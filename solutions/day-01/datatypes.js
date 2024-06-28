b0e5d8d73d214fe3f76c754fb97197f582dac590
Milo <JosephMRDominguez@gmail.com>
2024-06-27 18:14:30 -0700

    chore: exercise level1 complete

---
+++ solutions/day-01/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof(string))
+console.log(typeof(option))
+console.log(typeof(undef))
+console.log(typeof(nullVar))
+
---
+++ solutions/day-01/variable.js
@@ -0,0 +1,21 @@
+let string = ':)'
+let option = true
+let undef = undefined
+let nullVar = null
+
+let one
+let two
+let three
+let four
+
+let fName = "Joseph"
+let lName = "Dominguez"
+let ms = "Single"
+let country = "USA"
+let age = 23
+
+let f = "Joseph", l = "Dominguez", m = "Single", c = "USA", a = 23
+
+let myAge = 23, yourAge = 32
+
+console.log(myAge + '|' + yourAge)
